a144542489adf8e3f2214cf2d4306f980a4e7c7d
sammtcbn <sammtcbn@gmail.com>
2020-11-19 14:09:03 +0800

    import required package

--- sgcu.go
+++ sgcu.go
@@ -1,11 +1,14 @@
 package sgcu
 
 import (
+    "fmt"
     "strings"
+    "strconv"
+    "os"
 )
 
 func IndexAt (content string, sep string, startaddr int) int {
-    idx := strings.Index(content[startaddr:], sep)
+    idx := strings.Index (content[startaddr:], sep)
     if idx > -1 {
         idx += startaddr
     }
@@ -13,7 +16,7 @@ func IndexAt (content string, sep string, startaddr int) int {
 }
 
 func StringToFloat (str string) float64 {
-    floatvalue, err := strconv.ParseFloat(str, 64)
+    floatvalue, err := strconv.ParseFloat (str, 64)
     if err != nil {
         fmt.Println (err)
         os.Exit (2)
