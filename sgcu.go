6567485c20e74593a9292aac3f8379fe45151f86
sammtcbn <sammtcbn@gmail.com>
2020-11-19 14:04:16 +0800

    add StringToFloat function

--- sgcu.go
+++ sgcu.go
@@ -11,3 +11,12 @@ func IndexAt (content string, sep string, startaddr int) int {
     }
     return idx
 }
+
+func StringToFloat (str string) float64 {
+    floatvalue, err := strconv.ParseFloat(str, 64)
+    if err != nil {
+        fmt.Println (err)
+        os.Exit (2)
+    }
+    return floatvalue
+}
